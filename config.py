15e5196d1cda16da5d0ace04821860b2082feb3b
Lazy Developer <107105531+LazyDeveloperr@users.noreply.github.com>
2024-07-12 20:24:55 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,16 +1,15 @@
-import os, re
+import os
 from os import environ
 
-
+# Retrieving environment variables with correct syntax
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-API_ID = int(environ['API_ID'])
-API_HASH = environ['API_HASH']
-BOT_TOKEN = environ['BOT_TOKEN']
-OWNER = int(os.environ.get("OWNER"))
+API_ID = int(os.environ.get("API_ID", 0))  # Provide a default value of 0
+API_HASH = os.environ.get("API_HASH", "")
+OWNER = int(os.environ.get("OWNER", 0))  # Provide a default value of 0
 BOT_USERNAME = os.environ.get('BOT_USERNAME', "")
 
 FORCE_SUBS = os.environ.get("FORCE_SUBS", "HxBots")
-LOG_CHANNEL = int(environ.get('LOG_CHANNEL', 0))
+LOG_CHANNEL = int(os.environ.get('LOG_CHANNEL', 0))  # Provide a default value of 0
 
 DB_URL = os.environ.get("DB_URL", "")
 DB_NAME = os.environ.get("DB_NAME", "renamebot-premium")
@@ -18,12 +17,7 @@ DB_NAME = os.environ.get("DB_NAME", "renamebot-premium")
 STRING = os.environ.get("STRING", "")
 BOT_PIC = os.environ.get("BOT_PIC", "https://graph.org/file/cca849a2f63053fa3f622.jpg")
 
-
-
-
-
-# if you need to add verify system then dm me on telegram
-
+# Shortener settings
 SHORTNER_URL = os.environ.get("SHORTNER_URL", "")
 SHORTNER_API = os.environ.get("SHORTNER_API", "")
 TOKEN_TIMEOUT = os.environ.get("TOKEN_TIMEOUT", "")
