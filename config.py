fa7a2baec6f4fc0150c21e433ac1797c6aee4ef6
Lazy Developer <107105531+LazyDeveloperr@users.noreply.github.com>
2024-07-12 20:16:23 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,14 +1,16 @@
 import os, re
+from os import environ
 
 
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-API_ID = int(os.environ.get("API_ID"))
-API_HASH = os.environ.get("API_HASH", "")
+API_ID = int(environ['API_ID'])
+API_HASH = environ['API_HASH']
+BOT_TOKEN = environ['BOT_TOKEN']
 OWNER = int(os.environ.get("OWNER"))
 BOT_USERNAME = os.environ.get('BOT_USERNAME', "")
 
 FORCE_SUBS = os.environ.get("FORCE_SUBS", "HxBots")
-LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL"))
+LOG_CHANNEL = int(environ.get('LOG_CHANNEL', 0))
 
 DB_URL = os.environ.get("DB_URL", "")
 DB_NAME = os.environ.get("DB_NAME", "renamebot-premium")
